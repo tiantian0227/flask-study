836ead6588a7aa874364ee2efb98067ffe2e1897
tiantian0227 <1126298530@qq.com>
2017-10-22 18:46:42 +0800

    URL链接和加载静态文件

---
+++ static/js/index_page.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2017/10/22.
+ */
+
+alert('我是田甜');
---
+++ url_login.py
@@ -0,0 +1,17 @@
+# encoding:utf-8
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index_page.html')
+
+
+@app.route('/login/')
+def login():
+    return render_template('index_login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
